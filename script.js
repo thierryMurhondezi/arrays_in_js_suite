e4e0f9f106515f2f9ba77ac103770d8f6d295772
Thierry Murhondezi Bisimwa <thierrymurhondezi@gmail.com>
2023-09-21 00:58:09 +0100

    add : a new script

---
+++ script.js
@@ -0,0 +1,11 @@
+// Arrays in JavaScript have a lot of important features that you should know about. Here are some of the most important things to know:
+// - The length property: This property tells you how many elements are in the array.
+// - The push() and pop() methods: These methods add and remove elements from the end of the array.
+// - The sort() method: This method sorts the elements in the array alphabetically or numerically.
+// - The indexOf() method: This method finds the index of a specific element in the array.
+
+
+// Let's say you have an array called 'colors' that contains the string values "red", "blue", and "green". Here are some examples of how you could work with this array:
+// - colors.length: This would return the value 3, since there are three elements in the array.
+// - colors.push("yellow"): This would add the string "yellow" to the end of the array, so it would now have four elements.
+// - colors.pop(): This would remove the last element of the array, which is "green". The array would now have three elemen
